ec035f2a0579c0f4ed3ed30954dd33464e9b3ef8
lzypan812 <lzypan812@163.com>
2017-03-17 22:45:47 +0800

    create project

---
+++ src/lzypan812/leo/codec/bytetool/Byte2Number.java
@@ -0,0 +1,15 @@
+package lzypan812.leo.codec.bytetool;
+
+/**
+ * Byte to Number(short,int,long...)
+ *
+ * @author leo
+ * @date 2017年3月17日 下午10:22:41
+ * @version 1.0
+ */
+public class Byte2Number {
+
+	public static void stop(String a, String b) throws Exception {
+
+	}
+}
---
+++ src/lzypan812/leo/codec/bytetool/Byte2String.java
@@ -0,0 +1,11 @@
+package lzypan812.leo.codec.bytetool;
+/**
+ * TODO
+ *
+ * @author leo
+ * @date 2017年3月17日 下午10:26:44
+ * @version 1.0
+ */
+public class Byte2String {
+
+}
